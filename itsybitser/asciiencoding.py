885f942178fb8c75a30800d60d39bf24e77e47ff
Randy Gill <=>
2019-06-15 12:12:50 -0500

    completed asciiencoding

--- itsybitser/asciiencoding.py
+++ itsybitser/asciiencoding.py
@@ -4,11 +4,23 @@ import re
 
 
 class AsciiEncoding:
+    """ Common functionality between Atomixtream and Hextream classes """
 
     def __init__(self):
         self.comment_regex = re.compile("#.*$", re.MULTILINE)
 
     def compare(self, content1, content2):
+        """ Compares two strings
+
+        This compares two strings, while ignoring any whitespace characters
+        (space, tab, carriage return, line feed, form feed, or vertical
+        tab), and any comments (character sequences starting with "#" and
+        extending to the end-of-line.
+
+        - if strings are the same, will return -1
+        - if strings are different, will return the (0-based) index of the
+          first character where they differ """
+
         result = -1
         distilled_content1 = self.distill(content1)
         distilled_content2 = self.distill(content2)
@@ -19,6 +31,13 @@ class AsciiEncoding:
         return result
 
     def distill(self, content):
+        """ Removes whitespace and comments from string
+
+        This removes any whitespace characters (space, tab, carriage return,
+        line feed, form feed, and vertical tab), and any comments (character
+        sequences starting with "#" and extending to the end-of-line from the
+        passed-in string. """
+
         # Eliminate comments
         result = self.comment_regex.sub("", content)
         # Eliminate all whitespace (split by default splits on any WS char)
--- tests/test_asciiencoding.py
+++ tests/test_asciiencoding.py
@@ -1,50 +1,49 @@
 """ Unit Tests for class AsciiEncoder """
 
-import unittest
 from itsybitser import AsciiEncoding
 
+def test_distill_remove_whitespace():
+    """ Removal of spaces, tabs, LF, CR, FF, and VT """
+    ascii_encoding = AsciiEncoding()
+    result = ascii_encoding.distill("  A 1h\f3$BD  \n 4 []2\r\t  zy5C\v ")
+    assert result == "A1h3$BD4[]2zy5C"
 
-class TestAsciiEncoding(unittest.TestCase):
+def test_distill_remove_comments():
+    """ Removal of sequences starting w/"#" and extending to EOL """
+    ascii_encoding = AsciiEncoding()
+    result = ascii_encoding.distill(
+        "# comment line \n \t #comment after whitespace\nABCD12 # Inline Comment\n34\n#"
+    )
+    assert result == "ABCD1234"
 
-    def test_distill_remove_whitespace(self):
-        ascii_encoding = AsciiEncoding()
-        result = ascii_encoding.distill("  A 1h\f3$BD  \n 4 []2\r\t  zy5C\v ")
-        assert result == "A1h3$BD4[]2zy5C"
+def test_compare_simple_same():
+    """ Compare verbatum """
+    ascii_encoding = AsciiEncoding()
+    result = ascii_encoding.compare(
+        "vBIybIF&WBFE&WOF*f4((*N44wr9[]{}", "vBIybIF&WBFE&WOF*f4((*N44wr9[]{}"
+    )
+    assert result == -1
 
-    def test_distill_remove_comments(self):
-        ascii_encoding = AsciiEncoding()
-        result = ascii_encoding.distill(
-            "# comment line \n \t #comment after whitespace\nABCD12 # Inline Comment\n34\n#"
-        )
-        self.assertEqual(result, "ABCD1234")
+def test_compare_simple_different():
+    """ Compare different strings """
+    ascii_encoding = AsciiEncoding()
+    result = ascii_encoding.compare(
+        "vBIybIF&WBFE&WOF*f4((*N44wr9[]{}", "vBIybI&WBFE&WOF*f4((*N44wr9[]{}"
+    )
+    assert result == 6
 
-    def test_compare_simple_same(self):
-        ascii_encoding = AsciiEncoding()
-        result = ascii_encoding.compare(
-            "vBIybIF&WBFE&WOF*f4((*N44wr9[]{}", "vBIybIF&WBFE&WOF*f4((*N44wr9[]{}"
-        )
-        self.assertEqual(result, -1)
+def test_compare_whitespaced_same():
+    """ Compare same except for (ignored) whitespace """
+    ascii_encoding = AsciiEncoding()
+    result = ascii_encoding.compare(
+        "vBIybIF&WBFE&WOF*f\n4((*N44w r9[]{}", "vBIy bIF&W\rBFE&WOF*f4((*N44wr9[]{}"
+    )
+    assert result == -1
 
-    def test_compare_simple_different(self):
-        ascii_encoding = AsciiEncoding()
-        result = ascii_encoding.compare(
-            "vBIybIF&WBFE&WOF*f4((*N44wr9[]{}", "vBIybI&WBFE&WOF*f4((*N44wr9[]{}"
-        )
-        self.assertEqual(result, 6)
-
-    def test_compare_whitespaced_same(self):
-        ascii_encoding = AsciiEncoding()
-        result = ascii_encoding.compare(
-            "vBIybIF&WBFE&WOF*f\n4((*N44w r9[]{}", "vBIy bIF&W\rBFE&WOF*f4((*N44wr9[]{}"
-        )
-        self.assertEqual(result, -1)
-
-    def test_compare_whitespaced_different(self):
-        ascii_encoding = AsciiEncoding()
-        result = ascii_encoding.compare(
-            "vBIybIF&WBFE&WOF*f\n4((*N44w r9[-]{}", "vBIy bIF&W\rBFE&WOF*f4((*N44wr9[]{}"
-        )
-        self.assertEqual(result, 29)
-
-if __name__ == "__main__":
-    unittest.main()
+def test_compare_whitespaced_different():
+    """ Compare different strings w/ different (ignored) whitespace """
+    ascii_encoding = AsciiEncoding()
+    result = ascii_encoding.compare(
+        "vBIybIF&WBFE&WOF*f\n4((*N44w r9[-]{}", "vBIy bIF&W\rBFE&WOF*f4((*N44wr9[]{}"
+    )
+    assert result == 29
