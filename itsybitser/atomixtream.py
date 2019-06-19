390881952160cbc64cb069b82eb1c107038430b2
Randy Gill <=>
2019-06-18 22:37:41 -0500

    checkpoint on atomixtream

--- itsybitser/atomixtream.py
+++ itsybitser/atomixtream.py
@@ -14,6 +14,13 @@ class Atomixtream(AsciiEncoding):
 
     def __init__(self):
         super().__init__()
+        self.encoders = {
+            self.Encoding.BASIC64: self.__encode_basic64,
+            self.Encoding.OCTET_RUN: self.__encode_octet_run,
+            self.Encoding.SEXTET_RUN: self.__encode_sextet_run,
+            self.Encoding.SEXTET_STREAM: self.__encode_sextet_stream,
+            self.Encoding.TRIAD_STREAM: self.__encode_triad_stream
+        }
 
     def decode(self, content):
         """ Decode binary data from Hextream (ASCII) """
@@ -31,46 +38,15 @@ class Atomixtream(AsciiEncoding):
         length = len(content)
 
         if encoding == self.Encoding.SEXTET_STREAM:
-            result = "".join([chr(byte + OFFSET) for byte in content])
-
+            result = self.__encode_sextet_stream(content)
         elif encoding == self.Encoding.TRIAD_STREAM:
-            result = []
-            for i in range(0, (length + 1) // 2):
-                index = 2 * i
-                byte = content[index]
-                try:
-                    byte += content[index + 1] << 3
-                except IndexError:
-                    pass
-                result.append(chr(byte + OFFSET))
-            result = "".join(result)
-
+            result = self.__encode_triad_stream(content)
         elif encoding == self.Encoding.BASIC64:
-            result = []
-            for i in range(0, length):
-                if not i % 3:
-                    result.append(0)
-                    hi_bits_index = len(result) - 1
-                byte = content[i]
-                hi_bits_shift = 2 * (3 - i % 3)
-                result[hi_bits_index] += (byte & HI_BITS_MASK) >> hi_bits_shift
-                result.append(byte & SIX_BIT_MASK)
-            result = "".join([chr(byte + OFFSET) for byte in result])
-
+            result = self.__encode_basic64(content)
         elif encoding == self.Encoding.SEXTET_RUN:
-            if length:
-                result = chr(content[0] + OFFSET)
-            else:
-                result = ""
-
+            result = self.__encode_sextet_run(content)
         elif encoding == self.Encoding.OCTET_RUN:
-            if length:
-                result = (
-                    chr(content[0] // RADIX + OFFSET) +
-                    chr((content[0] & SIX_BIT_MASK) + OFFSET)
-                )
-            else:
-                result = ""
+            result = self.__encode_octet_run(content)
 
         result = self.__encode_header(encoding, length) + result
         return result
@@ -97,9 +73,65 @@ class Atomixtream(AsciiEncoding):
         BASIC64 = 7
 
     @staticmethod
+    def __encode_basic64(content):
+        result = []
+        length = len(content)
+        for i in range(0, length):
+            if not i % 3:
+                result.append(0)
+                hi_bits_index = len(result) - 1
+            byte = content[i]
+            hi_bits_shift = 2 * (3 - i % 3)
+            result[hi_bits_index] += (byte & HI_BITS_MASK) >> hi_bits_shift
+            result.append(byte & SIX_BIT_MASK)
+        result = "".join([chr(byte + OFFSET) for byte in result])
+        return result
+
+    @staticmethod
     def __encode_header(encoding, length):
         result = (
             chr(encoding.value + length // RADIX * 8 + OFFSET) +
             chr((length & SIX_BIT_MASK) + OFFSET)
         )
+        return result
+
+    @staticmethod
+    def __encode_octet_run(content):
+        length = len(content)
+        if length:
+            result = (
+                chr(content[0] // RADIX + OFFSET) +
+                chr((content[0] & SIX_BIT_MASK) + OFFSET)
+            )
+        else:
+            result = ""
+        return result
+
+    @staticmethod
+    def __encode_sextet_run(content):
+        length = len(content)
+        if length:
+            result = chr(content[0] + OFFSET)
+        else:
+            result = ""
+        return result
+
+    @staticmethod
+    def __encode_sextet_stream(content):
+        result = "".join([chr(byte + OFFSET) for byte in content])
+        return result
+
+    @staticmethod
+    def __encode_triad_stream(content):
+        length = len(content)
+        result = []
+        for i in range(0, (length + 1) // 2):
+            index = 2 * i
+            byte = content[index]
+            try:
+                byte += content[index + 1] << 3
+            except IndexError:
+                pass
+            result.append(chr(byte + OFFSET))
+        result = "".join(result)
         return result
