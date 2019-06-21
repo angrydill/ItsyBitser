371e59401adc40c035a543566b1a0761c785013c
Randy Gill <=>
2019-06-21 09:13:05 -0500

    converted varipacker from class to function lib

--- itsybitser/varipacker.py
+++ itsybitser/varipacker.py
@@ -1,7 +1,7 @@
 """ Text-encodes binary data, compressing where feasible """
 
 from enum import Enum
-from itsybitser.asciiencoding import AsciiEncoding
+from itsybitser import asciiencoding
 
 OFFSET = 48
 RADIX = 64
@@ -10,111 +10,92 @@ HIGH_BITS_MASK = 0b11000000
 GROUP_LENGTH = 3
 
 
-class VariPacker(AsciiEncoding):
-    """ Text-encodes binary data, compressing where feasible """
+class Encoding(Enum):
+    """ Indicates technique to use when encoding a chunk """
+    GAP = 0
+    OCTET_RUN = 1
+    SEXTET_RUN = 2
+    TRIAD_STREAM = 3
+    SEXTET_STREAM = 4
+    HEADER = 5
+    LINEAR64 = 7
 
-    def __init__(self):
-        super().__init__()
-        self.encoders = {
-            self.Encoding.LINEAR64: self.__encode_linear64,
-            self.Encoding.OCTET_RUN: self.__encode_octet_run,
-            self.Encoding.SEXTET_RUN: self.__encode_sextet_run,
-            self.Encoding.SEXTET_STREAM: self.__encode_sextet_stream,
-            self.Encoding.TRIAD_STREAM: self.__encode_triad_stream
-        }
 
-    def decode(self, content):
-        """ Decode binary data from Hextream (ASCII) """
-        result = None
-        return result
+def decode(content):
+    """ Decode binary data from Hextream (ASCII) """
+    result = None
+    return result
 
-    def encode(self, content):
-        """ Encode binary data as Hextream (ASCII) """
-        result = None
-        return result
+def encode(content):
+    """ Encode binary data as Hextream (ASCII) """
+    result = None
+    return result
 
-    def encode_chunk(self, content, encoding):
-        """ Encodes a byte sequence using specified encoding  """
-        result = self.encoders[encoding](content)
-        result = self.__encode_header(encoding, len(content)) + result
-        return result
+def encode_chunk(content, encoding):
+    """ Encodes a byte sequence using specified encoding  """
+    result = {
+        Encoding.LINEAR64: _encode_linear64,
+        Encoding.OCTET_RUN: _encode_octet_run,
+        Encoding.SEXTET_RUN: _encode_sextet_run,
+        Encoding.SEXTET_STREAM: _encode_sextet_stream,
+        Encoding.TRIAD_STREAM: _encode_triad_stream
+    }[encoding](content)
+    return _encode_header(encoding, len(content)) + result
 
-    def encode_gap(self, length):
-        """ Encodes instruction for decoder to skip forward length bytes """
-        result = self.__encode_header(self.Encoding.GAP, length)
-        return result
+def encode_gap(length):
+    """ Encodes instruction for decoder to skip forward length bytes """
+    return _encode_header(Encoding.GAP, length)
 
-    @staticmethod
-    def encode_terminus():
-        """ Encodes indicator that there are no more chunks to decode """
-        result = "00"
-        return result
+def encode_terminus():
+    """ Encodes indicator that there are no more chunks to decode """
+    return "00"
 
-    class Encoding(Enum):
-        """ Indicates technique to use when encoding a chunk """
-        GAP = 0
-        OCTET_RUN = 1
-        SEXTET_RUN = 2
-        TRIAD_STREAM = 3
-        SEXTET_STREAM = 4
-        HEADER = 5
-        LINEAR64 = 7
+def _encode_linear64(content):
+    result = []
+    for content_index, byte in enumerate(content):
+        group_position = content_index % GROUP_LENGTH
+        if not group_position:
+            high_bits_index = len(result)
+            result.append(0)
+        high_bits_shift = 2 * (GROUP_LENGTH - group_position)
+        result[high_bits_index] += (byte & HIGH_BITS_MASK) >> high_bits_shift
+        result.append(byte & SEXTET_MASK)
+    return "".join([chr(byte + OFFSET) for byte in result])
 
-    @staticmethod
-    def __encode_linear64(content):
-        result = []
-        for content_index, byte in enumerate(content):
-            group_position = content_index % GROUP_LENGTH
-            if not group_position:
-                high_bits_index = len(result)
-                result.append(0)
-            high_bits_shift = 2 * (GROUP_LENGTH - group_position)
-            result[high_bits_index] += (byte & HIGH_BITS_MASK) >> high_bits_shift
-            result.append(byte & SEXTET_MASK)
-        result = "".join([chr(byte + OFFSET) for byte in result])
-        return result
+def _encode_header(encoding, length):
+    result = (
+        chr(encoding.value + length // RADIX * 8 + OFFSET) +
+        chr((length & SEXTET_MASK) + OFFSET)
+    )
+    return result
 
-    @staticmethod
-    def __encode_header(encoding, length):
+def _encode_octet_run(content):
+    if content:
         result = (
-            chr(encoding.value + length // RADIX * 8 + OFFSET) +
-            chr((length & SEXTET_MASK) + OFFSET)
+            chr(content[0] // RADIX + OFFSET) +
+            chr((content[0] & SEXTET_MASK) + OFFSET)
         )
-        return result
+    else:
+        result = ""
+    return result
 
-    @staticmethod
-    def __encode_octet_run(content):
-        if content:
-            result = (
-                chr(content[0] // RADIX + OFFSET) +
-                chr((content[0] & SEXTET_MASK) + OFFSET)
-            )
-        else:
-            result = ""
-        return result
+def _encode_sextet_run(content):
+    if content:
+        result = chr(content[0] + OFFSET)
+    else:
+        result = ""
+    return result
 
-    @staticmethod
-    def __encode_sextet_run(content):
-        if content:
-            result = chr(content[0] + OFFSET)
-        else:
-            result = ""
-        return result
+def _encode_sextet_stream(content):
+    return "".join([chr(byte + OFFSET) for byte in content])
 
-    @staticmethod
-    def __encode_sextet_stream(content):
-        result = "".join([chr(byte + OFFSET) for byte in content])
-        return result
-
-    @staticmethod
-    def __encode_triad_stream(content):
-        result = []
-        for index in range(0, len(content), 2):
-            byte = content[index]
-            try:
-                byte += content[index + 1] << 3
-            except IndexError:
-                pass
-            result.append(chr(byte + OFFSET))
-        result = "".join(result)
-        return result
+def _encode_triad_stream(content):
+    result = []
+    for index in range(0, len(content), 2):
+        byte = content[index]
+        try:
+            byte += content[index + 1] << 3
+        except IndexError:
+            pass
+        result.append(chr(byte + OFFSET))
+    return "".join(result)
--- tests/test_varipacker.py
+++ tests/test_varipacker.py
@@ -1,168 +1,146 @@
-""" Unit test cases for VariPacker class """
+""" Unit test cases for varipacker class """
 
-from itsybitser.varipacker import VariPacker
+from itsybitser import varipacker
 
 def test_encode_sextet_stream():
-    varipacker = VariPacker()
     result = varipacker.encode_chunk(
         b"\x00\x01\x3e\x3f\x3f\x3e\x01\x00",
-        VariPacker.Encoding.SEXTET_STREAM
+        varipacker.Encoding.SEXTET_STREAM
     )
     assert result == "4801noon10"
 
 def test_encode_sextet_stream_empty():
-    varipacker = VariPacker()
     result = varipacker.encode_chunk(
         b"",
-        VariPacker.Encoding.SEXTET_STREAM
+        varipacker.Encoding.SEXTET_STREAM
     )
     assert result == "40"
 
 def test_encode_sextet_stream_maximal():
-    varipacker = VariPacker()
     result = varipacker.encode_chunk(
         b"\x2a" * 511,
-        VariPacker.Encoding.SEXTET_STREAM
+        varipacker.Encoding.SEXTET_STREAM
     )
     assert result == "lo" + "Z" * 511
 
 def test_encode_triad_stream():
-    varipacker = VariPacker()
     result = varipacker.encode_chunk(
         b"\x00\x01\x06\x07\x07\x06\x01\x00",
-        VariPacker.Encoding.TRIAD_STREAM
+        varipacker.Encoding.TRIAD_STREAM
     )
     assert result == "388ng1"
 
 def test_encode_triad_stream_empty():
-    varipacker = VariPacker()
     result = varipacker.encode_chunk(
         b"",
-        VariPacker.Encoding.TRIAD_STREAM
+        varipacker.Encoding.TRIAD_STREAM
     )
     assert result == "30"
 
 def test_encode_triad_stream_maximal():
-    varipacker = VariPacker()
     result = varipacker.encode_chunk(
         b"\x01" * 511,
-        VariPacker.Encoding.TRIAD_STREAM
+        varipacker.Encoding.TRIAD_STREAM
     )
     assert result == "ko" + ("9" * 255) + "1"
 
 def test_encode_linear64_empty():
-    varipacker = VariPacker()
     result = varipacker.encode_chunk(
         b"",
-        VariPacker.Encoding.LINEAR64
+        varipacker.Encoding.LINEAR64
     )
     assert result == "70"
 
 def test_encode_linear64_length1():
-    varipacker = VariPacker()
     result = varipacker.encode_chunk(
         b"\xff",
-        VariPacker.Encoding.LINEAR64
+        varipacker.Encoding.LINEAR64
     )
     assert result == "713o"
 
 def test_encode_linear64_length2():
-    varipacker = VariPacker()
     result = varipacker.encode_chunk(
         b"\xff\x77",
-        VariPacker.Encoding.LINEAR64
+        varipacker.Encoding.LINEAR64
     )
     assert result == "727og"
 
 def test_encode_linear64_length3():
-    varipacker = VariPacker()
     result = varipacker.encode_chunk(
         b"\xff\x77\xaa",
-        VariPacker.Encoding.LINEAR64
+        varipacker.Encoding.LINEAR64
     )
     assert result == "73WogZ"
 
 def test_encode_linear64_length4():
-    varipacker = VariPacker()
     result = varipacker.encode_chunk(
         b"\xff\x77\xaa\xfe",
-        VariPacker.Encoding.LINEAR64
+        varipacker.Encoding.LINEAR64
     )
     assert result == "74WogZ3n"
 
 def test_encode_linear64_maximal():
-    varipacker = VariPacker()
     result = varipacker.encode_chunk(
         b"\xff" * 511,
-        VariPacker.Encoding.LINEAR64
+        varipacker.Encoding.LINEAR64
     )
     assert result == "o" * 682 + "3o"
 
 def test_encode_sextet_run_empty():
-    varipacker = VariPacker()
     result = varipacker.encode_chunk(
         b"",
-        VariPacker.Encoding.SEXTET_RUN
+        varipacker.Encoding.SEXTET_RUN
     )
     assert result == "20"
 
 def test_encode_sextet_run():
-    varipacker = VariPacker()
     result = varipacker.encode_chunk(
         b"\x2a" * 7,
-        VariPacker.Encoding.SEXTET_RUN
+        varipacker.Encoding.SEXTET_RUN
     )
     assert result == "27Z"
 
 def test_encode_sextet_run_maximal():
-    varipacker = VariPacker()
     result = varipacker.encode_chunk(
         b"\x3e" * 511,
-        VariPacker.Encoding.SEXTET_RUN
+        varipacker.Encoding.SEXTET_RUN
     )
     assert result == "jon"
 
 def test_encode_octet_run_empty():
-    varipacker = VariPacker()
     result = varipacker.encode_chunk(
         b"",
-        VariPacker.Encoding.OCTET_RUN
+        varipacker.Encoding.OCTET_RUN
     )
     assert result == "10"
 
 def test_encode_octet_run():
-    varipacker = VariPacker()
     result = varipacker.encode_chunk(
         b"\xfa" * 7,
-        VariPacker.Encoding.OCTET_RUN
+        varipacker.Encoding.OCTET_RUN
     )
     assert result == "173j"
 
 def test_encode_octet_run_maximal():
-    varipacker = VariPacker()
     result = varipacker.encode_chunk(
         b"\xdd" * 511,
-        VariPacker.Encoding.OCTET_RUN
+        varipacker.Encoding.OCTET_RUN
     )
     assert result == "io3M"
 
 def test_encode_gap_empty():
-    varipacker = VariPacker()
     result = varipacker.encode_gap(0)
     assert result == "00"
 
 def test_encode_gap():
-    varipacker = VariPacker()
     result = varipacker.encode_gap(7)
     assert result == "07"
 
 def test_encode_gap_maximal():
-    varipacker = VariPacker()
     result = varipacker.encode_gap(511)
     assert result == "ho"
 
 def test_encode_terminus():
-    varipacker = VariPacker()
     result = varipacker.encode_terminus()
     assert result == "00"
 
